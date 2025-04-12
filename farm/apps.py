674a305a570c0b276893b546b9c261b969283107
adlynfakhreyz <adlynuniacc@gmail.com>
2025-04-13 04:31:58 +0700

    update: design update

--- farm/apps.py
+++ farm/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AgriHubConfig(AppConfig):
+class FarmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agri_hub'
+    name = 'farm'
